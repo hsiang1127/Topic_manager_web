9793df73a4635c36d9bb7b0deabdf93aeff8d704
hsiang1127 <10936002@ntub.edu.tw>
2021-05-30 19:38:28 +0800

    量表管理介面

--- scale.php
+++ scale.php
@@ -28,8 +28,8 @@
 		</p>
 		<center>
 			<button class='button' id='teacher' name='teacher' onclick="show_scalemanagement('scale_frequency.php')"><b>量表頻率</b></button>
-			<button class='button' id='teacher' name='teacher' onclick="show_scalemanagement('')"><b>社會適應量表</b></button>
-			<button class='button' id='teacher' name='teacher' onclick="show_scalemanagement('')"><b>情緒障礙量表</b></button>
+			<button class='button' id='teacher' name='teacher' onclick="show_scalemanagement('adaptation_scale.php')"><b>社會適應量表</b></button>
+			<button class='button' id='teacher' name='teacher' onclick="show_scalemanagement('mood_disorders_scale.php')"><b>情緒障礙量表</b></button>
 		</center>
 		</p>
 		<div id='show_scalemanagement'></div>
