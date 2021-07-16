a981168c6fa9b8339f19a8a6e16155c3b84b9c66
hsiang1127 <10936002@ntub.edu.tw>
2021-07-17 00:04:53 +0800

    修改學生遊玩紀錄顯示頁面、新增刪除功能

--- play_history.php
+++ play_history.php
@@ -8,7 +8,20 @@
 		<link rel="stylesheet" type="text/css" href="css/table.css">
 		<link rel="stylesheet" type="text/css" href="css/button.css">
 		<style>
-		
+			.button_upd {
+				border: 2px solid #5599FF;
+			}
+			.button_upd:hover {
+				background-color: #5599FF;
+				color: white;
+			}
+			.button_del {
+				border: 2px solid #FF3333;
+			}
+			.button_del:hover {
+				background-color: #FF3333;
+				color: white;
+			}
 		</style>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<script>
@@ -17,16 +30,23 @@
 	</header>
 	<body>
 		</p>
-		<table align='center' width='60%'>
-			<tr>
-				<th width="25%">學號</th>
-				<th width="25%">遊戲種類</th>
-				<th width="25%">遊玩時間</th>
-				<th width="25%">時長</th>
-			</tr>
+		<table align='center' width='55%'>
 			<?php
 				$sel_play_history=mysqli_query($conn,"select * from `play_history`");
-				while($sel_play_history_ok=mysqli_fetch_array($sel_play_history)){
+				$n = mysqli_num_rows($sel_play_history);
+				if($n==0){
+					echo "<h1 align='center'><font color='#FF3333'>目前無紀錄資料!!</font></h1>";
+				}else{
+			?>		
+				<tr>
+					<th width='20%'>學號</th>
+					<th width='20%'>遊戲種類</th>
+					<th width='25%'>遊玩時間</th>
+					<th width='20%'>時長</th>
+					<th></th>
+				</tr>
+			<?php		
+					while($sel_play_history_ok=mysqli_fetch_array($sel_play_history)){
 					echo "
 						<tr>
 							<td>$sel_play_history_ok[student_id]</td>
@@ -39,8 +59,12 @@
 					echo "	
 							<td>$sel_play_history_ok[play_date]</td>
 							<td>$sel_play_history_ok[play_time]分</td>
+							<td>
+								<button class='button button_del' onclick=\"show('play_history_del.php?id=$sel_play_history_ok[play_id]')\"><b>刪除</b></button>
+							</td>
 						</tr>
 					";
+				}
 				}
 			?>
 		</table>
