c5e352c1729aac5ca606812f5d6aa0b45f8110f3
hsiang1127 <10936002@ntub.edu.tw>
2021-07-16 18:58:27 +0800

    修改學生資料修改內容

--- student_upd.php
+++ student_upd.php
@@ -8,13 +8,20 @@
 		<link rel="stylesheet" type="text/css" href="css/table.css">
 		<link rel="stylesheet" type="text/css" href="css/button.css">
 		<style>
-		
+			.button_upd {
+				border: 2px solid #5599FF;
+			}
+			.button_upd:hover {
+				background-color: #5599FF;
+				color: white;
+			}
 		</style>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<script>
 			function stu_upd(){
 				var user=document.getElementById("user").value;
 				var password=document.getElementById("password").value;
+				var stuid=document.getElementById("stuid").value;
 				var name=document.getElementById("name").value;
 				var s_year=document.getElementById("s_year").value;
 				var s_class=document.getElementById("s_class").value;
@@ -24,6 +31,10 @@
 				var f_year=document.getElementById("f_year").value;
 				if(password==""){
 					alert('密碼未輸入!!');
+					return false;
+				}
+				if(stuid==""){
+					alert('學號未輸入!!');
 					return false;
 				}
 				if(name==""){
@@ -46,19 +57,19 @@
 					alert('爸爸年齡未輸入!!');
 					return false;
 				}
-				window.location.href="student_upd_ok.php?user="+user+"&password="+password+"&name="+name+"&s_year="+s_year+"&s_class="+s_class+"&sex="+sex+"&birthday="+birthday+"&m_year="+m_year+"&f_year="+f_year;
+				window.location.href="student_upd_ok.php?user="+user+"&password="+password+"&stuid="+stuid+"&name="+name+"&s_year="+s_year+"&s_class="+s_class+"&sex="+sex+"&birthday="+birthday+"&m_year="+m_year+"&f_year="+f_year;
 			}
 		</script>
 	</header>
 	<body>
 		<?php
-			$sel=mysqli_query($conn,"select * from `student` where `student_id`='$_GET[user]'");
+			$sel=mysqli_query($conn,"select * from `student` where `user_id`='$_GET[user]'");
 			if($sel_ok=mysqli_fetch_array($sel)){
 		?>
 		</p>
 		<table align='center' width='30%'>
 			<tr>
-				<th width='40%'>學號</th>
+				<th width='40%'>學生帳號</th>
 				<td><?php echo $_GET['user'];?></td>
 				<input type="hidden" id="user" name="user" value="<?php echo $_GET['user'];?>">
 			</tr>
@@ -67,16 +78,12 @@
 				<td><input type='text' id='password' name='password' style="text-align:center;" value='<?php echo $sel_ok['password'];?>'></td>
 			</tr>
 			<tr>
+				<th>學號</th>
+				<td><input type='text' id='stuid' name='stuid' style="text-align:center;" value='<?php echo $sel_ok['student_id'];?>'></td>
+			</tr>
+			<tr>
 				<th>姓名</th>
 				<td><input type='text' id='name' name='name' style="text-align:center;" value='<?php echo $sel_ok['student_name'];?>'></td>
-			</tr>
-			<tr>
-				<th>年級</th>
-				<td><input type='text' id='s_year' name='s_year' style="text-align:center;" value='<?php echo $sel_ok['student_year'];?>'></td>
-			</tr>
-			<tr>
-				<th>班級</th>
-				<td><input type='text' id='s_class' name='s_class' style="text-align:center;" value='<?php echo $sel_ok['student_class'];?>'></td>
 			</tr>
 			<tr>
 				<th>性別</th>
@@ -87,12 +94,16 @@
 							<input type='radio' id='sex' name='sex' value='男' checked>男
 							<input type='radio' id='sex1' name='sex' value='女'>女
 					<?php
-						}else{
+						}else if($sel_ok['sex']=="女"){
 					?>
 							<input type='radio' id='sex' name='sex' value='男'>男
 							<input type='radio' id='sex1' name='sex' value='女' checked>女
 					<?php		
-							
+						}else{
+					?>
+							<input type='radio' id='sex' name='sex' value='男'>男
+							<input type='radio' id='sex1' name='sex' value='女'>女
+					<?php
 						}
 					?>
 
@@ -103,15 +114,7 @@
 				<td><input type='date' id='birthday' name='birthday' style="text-align:center;" value='<?php echo $sel_ok['birthday'];?>'></td>
 			</tr>
 			<tr>
-				<th>媽媽年齡</th>
-				<td><input type='text' id='m_year' name='m_year' style="text-align:center;" value='<?php echo $sel_ok['mom_year'];?>'></td>
-			</tr>
-			<tr>
-				<th>爸爸年齡</th>
-				<td><input type='text' id='f_year' name='f_year' style="text-align:center;" value='<?php echo $sel_ok['father_year'];?>'></td>
-			</tr>
-			<tr>
-				<td colspan='2'><button class='button' onclick="stu_upd();"><b>確認修改</b></button></td>
+				<td colspan='2'><button class='button button_upd' onclick="stu_upd();"><b>確認修改</b></button></td>
 			</tr>
 		</table>
 		<?php		
--- student_upd_ok.php
+++ student_upd_ok.php
@@ -1,7 +1,7 @@
 <?php
 	session_start();
 	include("conn.php");
-	$sql="update `student` set `password`='$_GET[password]',`student_name`='$_GET[name]', `student_year`='$_GET[s_year]', `student_class`='$_GET[s_class]', `mom_year`='$_GET[m_year]', `father_year`='$_GET[f_year]', `birthday`='$_GET[birthday]', `sex`='$_GET[sex]' where `student_id`='$_GET[user]'";
+	$sql="update `student` set `password`='$_GET[password]',`student_id`='$_GET[stuid]',`student_name`='$_GET[name]', `student_year`='$_GET[s_year]', `student_class`='$_GET[s_class]', `mom_year`='$_GET[m_year]', `father_year`='$_GET[f_year]', `birthday`='$_GET[birthday]', `sex`='$_GET[sex]' where `user_id`='$_GET[user]'";
 	$upd=mysqli_query($conn,$sql);
 	if($upd){
 		header("Location:main.php");
