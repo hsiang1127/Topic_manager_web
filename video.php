6e99135c9d153c4e9735f12098ba80e3fa5b55dc
hsiang1127 <10936002@ntub.edu.tw>
2021-07-17 00:05:20 +0800

    修改學生新增紀錄顯示頁面、新增刪除功能

--- video.php
+++ video.php
@@ -8,7 +8,20 @@
 		<link rel="stylesheet" type="text/css" href="css/table.css">
 		<link rel="stylesheet" type="text/css" href="css/button.css">
 		<style>
-		
+			.button_upd {
+				border: 2px solid #5599FF;
+			}
+			.button_upd:hover {
+				background-color: #5599FF;
+				color: white;
+			}
+			.button_del {
+				border: 2px solid #FF3333;
+			}
+			.button_del:hover {
+				background-color: #FF3333;
+				color: white;
+			}
 		</style>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<script>
@@ -28,8 +41,9 @@
 			?>
 					<tr>
 						<th width="30%">學號</th>
-						<th width="40%">影片網址</th>
+						<th width="30%">影片網址</th>
 						<th width="30%">上傳時間</th>
+						<th width="10%"></th>
 					</tr>	
 			<?php
 				}
@@ -37,8 +51,11 @@
 					echo "
 						<tr>
 							<td>$sel_video_ok[student_id]</td>
-							<td>$sel_video_ok[video_url]</td>
+							<td><a href='$sel_video_ok[video_url]'>影片連結</a></td>
 							<td>$sel_video_ok[write_time]</td>
+							<td>
+								<button class='button button_del' onclick=\"show('video_del.php?id=$sel_video_ok[video_id]')\"><b>刪除</b></button>
+							</td>
 						</tr>
 					";
 				}
