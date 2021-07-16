9fa9b677192dee155cf1555ff74a09b35e324c21
hsiang1127 <10936002@ntub.edu.tw>
2021-07-16 18:48:17 +0800

    修改學生管理顯示內容

--- student_management.php
+++ student_management.php
@@ -32,36 +32,52 @@
 		</p>
 		<table align='center' width='60%'>
 			<tr>
-				<th>學號</th>
+				<th>學生帳號</th>
+				<th>學號(班級座號)</th>
 				<th>姓名</th>
-				<th>年級</th>
-				<th>班級</th>
 				<th>性別</th>
 				<th>生日</th>
-				<th>爸爸年齡</th>
-				<th>媽媽年齡</th>
-				<th></th>
+				<th width="25%"></th>
 			</tr>
 			<?php
-				$sel_student=mysqli_query($conn,"select * from `student`");
-				while($sel_student_ok=mysqli_fetch_array($sel_student)){
-					echo "
-						<tr>
-							<td>$sel_student_ok[student_id]</td>
-							<td>$sel_student_ok[student_name]</td>
-							<td>$sel_student_ok[student_year]</td>
-							<td>$sel_student_ok[student_class]</td>
-							<td>$sel_student_ok[sex]</td>
-							<td>$sel_student_ok[birthday]</td>
-							<td>$sel_student_ok[father_year]</td>
-							<td>$sel_student_ok[mom_year]</td>	
-							<td>
-								<button class='button button_upd' onclick=\"show('student_upd.php?user=$sel_student_ok[student_id]')\"><b>修改</b></button>
-								<button class='button button_del' onclick=\"show('student_del_ok.php?user=$sel_student_ok[student_id]')\"><b>刪除</b></button>
-							</td>
-						</tr>
-					";
+				$sel_student=mysqli_query($conn,"SELECT `s`.*,`r`.`student_id` as `r_student_id` FROM `student` as `s` 
+												 LEFT JOIN `relationship` as `r` on `r`.`student_id`=`s`.`user_id` 
+												 ORDER BY `s`.`user_id`");
+				if(mysqli_num_rows($sel_student)>0){
+					while($sel_student_ok=mysqli_fetch_array($sel_student)){
+						echo "
+							<tr>
+								<td>$sel_student_ok[user_id]</td>
+								<td>$sel_student_ok[student_id]</td>
+								<td>$sel_student_ok[student_name]</td>
+								<td>$sel_student_ok[sex]</td>
+								<td>$sel_student_ok[birthday]</td>
+						";
+						if($sel_student_ok['r_student_id']==null){
+							echo "
+								<td align='left'>
+									<button class='button button_upd' onclick=\"show_management('student_upd.php?user=$sel_student_ok[user_id]')\"><b>修改</b></button>
+									<button class='button button_del' onclick=\"show_management('student_del_ok.php?user=$sel_student_ok[user_id]')\"><b>刪除</b></button>
+									<button class='button' onclick=\"show_management('relationship_ins.php?id=$sel_student_ok[user_id]')\"><b>親子綁定</b></button>
+								</td>
+							";
+						}else{
+							echo "
+								<td align='left'>
+									<button class='button button_upd' onclick=\"show_management('student_upd.php?user=$sel_student_ok[user_id]')\"><b>修改</b></button>
+									<button class='button button_del' onclick=\"show_management('student_del_ok.php?user=$sel_student_ok[user_id]')\"><b>刪除</b></button>
+									<button class='button' onclick=\"show_management('relationship_sel.php?id=$sel_student_ok[user_id]')\"><b>查看親子資料</b></button>
+								</td>
+							";
+						}
+						echo "
+							</tr>
+						";
+					}
+				}else{
+					echo "<h1 align='center'><font color='#FF3333'>目前學生資料!!</font></h1>";
 				}
+				
 			?>
 		</table>
 	</body>
