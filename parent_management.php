e18f5198640fd616e3128b7ab1ffc38270b46ac2
hsiang1127 <10936002@ntub.edu.tw>
2021-07-16 19:15:08 +0800

    修改家長資料管理呈現內容

--- parent_management.php
+++ parent_management.php
@@ -30,28 +30,58 @@
 	</header>
 	<body>
 		</p>
-		<table align='center' width='40%'>
-			<tr>
-				<th width='25%'>帳號</th>
-				<th width='25%'>小孩</th>
-				<th width='25%'>姓名</th>
-				<th width='25%'></th>
-			</tr>
+		<table align='center' width='20%'>
+			<!--<tr>
+				<th width="50%">家長帳號</th>
+				<th>學生帳號</th>
+				<th>與學生的關係</th>
+				<th>是否與學生同住</th>
+				<th>在家誰對孩子照顧或管教</th>
+				<th>家中排行</th>
+				<th>兄弟姐妹數</th>
+				<th>家中收入來源</th>
+				<th></th>
+				
+			</tr>-->
 			<?php
-				$sel_parent=mysqli_query($conn,"select * from `parent`");
-				while($sel_parent_ok=mysqli_fetch_array($sel_parent)){
+				/*$sel_parent=mysqli_query($conn,"SELECT `r`.*,`p`.`password` FROM `parent` as `p` 
+												INNER JOIN `relationship` as `r` on `r`.`parent_id`=`p`.`parent_id`");*/
+												
+				$sel_parent=mysqli_query($conn,"SELECT * FROM `parent`");
+				if(mysqli_num_rows($sel_parent)>0){
 					echo "
 						<tr>
-							<td>$sel_parent_ok[parent_id]</td>
-							<td>$sel_parent_ok[child]</td>
-							<td>$sel_parent_ok[parent_name]</td>
-							<td>
-								<button class='button button_upd' onclick=\"show('parent_upd.php?user=$sel_parent_ok[parent_id]')\"><b>修改</b></button>
-								<button class='button button_del' style='' onclick=\"show('parent_del_ok.php?user=$sel_parent_ok[parent_id]')\"><b>刪除</b></button>
-							</td>
+							<th width='50%'>家長帳號</th>
+							<th></th>
+							
 						</tr>
 					";
+					while($sel_parent_ok=mysqli_fetch_array($sel_parent)){
+						echo "
+							<tr>
+								<td>$sel_parent_ok[parent_id]</td>
+							";
+						/*echo "	
+								<td>$sel_parent_ok[student_id]</td>
+								<td>$sel_parent_ok[relationship]</td>
+								<td>$sel_parent_ok[cohabitation]</td>
+								<td>$sel_parent_ok[discipline]</td>
+								<td>$sel_parent_ok[home_order]</td>
+								<td>$sel_parent_ok[brothers_sisters]</td>
+								<td>$sel_parent_ok[income]</td>
+						";*/
+						echo "
+								<td>
+									<button class='button button_upd' onclick=\"show('parent_upd.php?user=$sel_parent_ok[parent_id]')\"><b>修改</b></button>
+									<button class='button button_del' style='' onclick=\"show('parent_del_ok.php?user=$sel_parent_ok[parent_id]')\"><b>刪除</b></button>
+								</td>
+							</tr>
+						";
+					}
+				}else{
+					echo "<h1 align='center'><font color='#FF3333'>目前無家長資料!!</font></h1>";
 				}
+				
 			?>
 		</table>
 	</body>
