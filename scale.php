8e0c20ea38c95ae948890fa63878716d644af0bb
hsiang1127 <10936002@ntub.edu.tw>
2021-07-17 13:20:34 +0800

    新增互動量表管理頁面

--- scale.php
+++ scale.php
@@ -30,6 +30,7 @@
 			<button class='button' id='teacher' name='teacher' onclick="show_scalemanagement('scale_frequency.php')"><b>量表頻率</b></button>
 			<button class='button' id='teacher' name='teacher' onclick="show_scalemanagement('adaptation_scale.php')"><b>社會適應量表</b></button>
 			<button class='button' id='teacher' name='teacher' onclick="show_scalemanagement('mood_disorders_scale.php')"><b>情緒障礙量表</b></button>
+			<button class='button' id='teacher' name='teacher' onclick="show_scalemanagement('interactive_scale.php')"><b>互動量表</b></button>
 		</center>
 		</p>
 		<div id='show_scalemanagement'></div>
