d86a0a09dd7e6df57c732d48baf8477b7f26a3aa
hsiang1127 <10936002@ntub.edu.tw>
2021-07-19 22:12:06 +0800

    修改家長與孩子的互動填寫紀錄顯示

--- interactive_scale_w.php
+++ interactive_scale_w.php
@@ -32,15 +32,93 @@
 				display: none; 
 				position: absolute; 
 				top: 23%; 
-				left: 72%; 
-				width: 25%; 
-				height: 32%; 
+				left: 75%; 
+				width: 20%; 
+				height: 50%; 
 				padding: 20px; 
 				border: 5px solid #FFBB66; 
 				background-color: #FFF6CC; 
 				z-index:1002; 
 				overflow: auto; 
+			}
+			.white_content1 { 
+				display: none; 
+				position: absolute; 
+				top: 22%; 
+				left: 32%; 
+				width: 12%; 
+				height: 4%; 
+				padding: 5px; 
+				border: 5px solid #FFBB66; 
+				background-color: #FFF6CC; 
+				z-index:1002; 
+				overflow: auto; 
+			}
+			.white_content2 { 
+				display: none; 
+				position: absolute; 
+				top: 22%; 
+				left: 35%; 
+				width: 25%; 
+				height: 4%; 
+				padding: 5px; 
+				border: 5px solid #FFBB66; 
+				background-color: #FFF6CC; 
+				z-index:1002; 
+				overflow: auto; 
 			} 
+			.white_content3 { 
+				display: none; 
+				position: absolute; 
+				top: 22%; 
+				left: 38%; 
+				width: 35%; 
+				height: 4%; 
+				padding: 5px; 
+				border: 5px solid #FFBB66; 
+				background-color: #FFF6CC; 
+				z-index:1002; 
+				overflow: auto; 
+			} 
+			.white_content4 { 
+				display: none; 
+				position: absolute; 
+				top: 22%; 
+				left: 41%; 
+				width: 25%; 
+				height: 4%; 
+				padding: 5px; 
+				border: 5px solid #FFBB66; 
+				background-color: #FFF6CC; 
+				z-index:1002; 
+				overflow: auto; 
+			}
+			.white_content5 { 
+				display: none; 
+				position: absolute; 
+				top: 22%; 
+				left: 44%; 
+				width: 30%; 
+				height: 4%; 
+				padding: 5px; 
+				border: 5px solid #FFBB66; 
+				background-color: #FFF6CC; 
+				z-index:1002; 
+				overflow: auto; 
+			} 	
+			.white_content6 { 
+				display: none; 
+				position: absolute; 
+				top: 22%; 
+				left: 47%; 
+				width: 20%; 
+				height: 4%; 
+				padding: 5px; 
+				border: 5px solid #FFBB66; 
+				background-color: #FFF6CC; 
+				z-index:1002; 
+				overflow: auto; 
+			} 	
 		</style>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<script>
@@ -53,10 +131,10 @@
 			量表數值　1-總是這樣　2-經常這樣　3-有時這樣　4-很少這樣　5-從不這樣　
 			<button class='button' onclick="document.getElementById('light').style.display='block';document.getElementById('fade').style.display='block'"><b>量表題目</b></button>
 		<h3>
-		<table align='center' width='40%'>
+		<table align='center' width='50%'>
 			
 			<?php
-				$sel_interactive_scale_w=mysqli_query($conn,"SELECT * FROM `interactive_scale_w` ORDER BY `write_time` desc");
+				$sel_interactive_scale_w=mysqli_query($conn,"SELECT * FROM `interactive_scale_w` ORDER BY `parent_id`,`number`");
 				$n = mysqli_num_rows($sel_interactive_scale_w);
 				if($n==0){
 					echo "<h1 align='center'><font color='#FF3333'>目前無紀錄資料!!</font></h1>";
@@ -64,13 +142,14 @@
 			?>
 					<tr>
 						<th>家長帳號</th>
-						<th>Q1</th>
-						<th>Q2</th>
-						<th>Q3</th>
-						<th>Q4</th>
-						<th>Q5</th>
-						<th>Q6</th>
+						<th onMouseOver="document.getElementById('light1').style.display='block';document.getElementById('fade').style.display='block'" onMouseOut="document.getElementById('light1').style.display='none';document.getElementById('fade').style.display='none'">Q1</th>
+						<th onMouseOver="document.getElementById('light2').style.display='block';document.getElementById('fade').style.display='block'" onMouseOut="document.getElementById('light2').style.display='none';document.getElementById('fade').style.display='none'">Q2</th>
+						<th onMouseOver="document.getElementById('light3').style.display='block';document.getElementById('fade').style.display='block'" onMouseOut="document.getElementById('light3').style.display='none';document.getElementById('fade').style.display='none'">Q3</th>
+						<th onMouseOver="document.getElementById('light4').style.display='block';document.getElementById('fade').style.display='block'" onMouseOut="document.getElementById('light4').style.display='none';document.getElementById('fade').style.display='none'">Q4</th>
+						<th onMouseOver="document.getElementById('light5').style.display='block';document.getElementById('fade').style.display='block'" onMouseOut="document.getElementById('light5').style.display='none';document.getElementById('fade').style.display='none'">Q5</th>
+						<th onMouseOver="document.getElementById('light6').style.display='block';document.getElementById('fade').style.display='block'" onMouseOut="document.getElementById('light6').style.display='none';document.getElementById('fade').style.display='none'">Q6</th>
 						<th>填寫時間</th>
+						<th>次數</th>
 						<th></th>
 					</tr>
 			<?php		
@@ -86,6 +165,7 @@
 							<td>$sel_interactive_scale_w_ok[q5]</td>
 							<td>$sel_interactive_scale_w_ok[q6]</td>
 							<td>$sel_interactive_scale_w_ok[write_time]</td>
+							<td>$sel_interactive_scale_w_ok[number]</td>
 							<td><button class='button button_del' onclick=\"show('interactive_scale_data_del.php?id=$sel_interactive_scale_w_ok[w_scale_id]')\"><b>刪除</b></button></td>
 						</tr>	
 					";
@@ -115,6 +195,36 @@
 			<p/>
 			<a href = "javascript:void(0)" onclick = "document.getElementById('light').style.display='none';document.getElementById('fade').style.display='none'">關閉視窗</a>
 		</div> 
+		<div id="light1" class="white_content1" align='center'>
+			<?php 
+				echo "Q1.$arrayq[0]";
+			?>	
+		</div>
+		<div id="light2" class="white_content2" align='center'>
+			<?php 
+				echo "Q2.$arrayq[1]";
+			?>	
+		</div>
+		<div id="light3" class="white_content3" align='center'>
+			<?php 
+				echo "Q3.$arrayq[2]";
+			?>	
+		</div>
+		<div id="light4" class="white_content4" align='center'>
+			<?php 
+				echo "Q4.$arrayq[3]";
+			?>	
+		</div>
+		<div id="light5" class="white_content5" align='center'>
+			<?php 
+				echo "Q5.$arrayq[4]";
+			?>	
+		</div>
+		<div id="light6" class="white_content6" align='center'>
+			<?php 
+				echo "Q6.$arrayq[5]";
+			?>	
+		</div>
         <div id="fade" class="black_overlay"></div>
 	</body>
 </html>
