eed809c6b688bae22f695b78bf1f52e38882a827
hsiang1127 <10936002@ntub.edu.tw>
2021-09-16 09:23:16 +0800

    學生遊戲紀錄頁籤跳轉

--- play_history.php
+++ play_history.php
@@ -34,11 +34,11 @@
 				}
 				/*var stu=document.getElementById("stu").value;
 				if(stu==""){
-					show_apphistory('mood_thermometer_w.php?n='+s1);
+					show_apphistory('play_history.php?n='+s1);
 				}else if(stu!=""){
-					show_apphistory('mood_thermometer_w.php?stu='+stu+"&n="+s1);
+					show_apphistory('play_history.php?stu='+stu+"&n="+s1);
 				}*/
-				show_apphistory('mood_thermometer_w.php?n='+s1);
+				show_apphistory('play_history.php?n='+s1);
 			}
 		</script>
 	</header>
