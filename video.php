436523919493c3e4d7579ab92de0d0582ed35324
hsiang1127 <10936002@ntub.edu.tw>
2021-09-16 09:41:33 +0800

    修改影片新增紀錄頁籤跳轉

--- video.php
+++ video.php
@@ -25,7 +25,21 @@
 		</style>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<script>
-
+			function qq(){
+				var s1;
+				if(document.getElementById("s1")){
+					s1=document.getElementById("s1").value;
+				}else{
+					s1=1;
+				}
+				/*var stu=document.getElementById("stu").value;
+				if(stu==""){
+					show_apphistory('play_history.php?n='+s1);
+				}else if(stu!=""){
+					show_apphistory('play_history.php?stu='+stu+"&n="+s1);
+				}*/
+				show_apphistory('play_history.php?n='+s1);
+			}
 		</script>
 	</header>
 	<body>
@@ -33,7 +47,13 @@
 		<table align='center' width='50%'>
 			
 			<?php
-				$sel_video=mysqli_query($conn,"SELECT * FROM `video` ORDER BY `write_time` ASC ");
+				if(isset($_GET['n'])){
+					$n=($_GET['n']*10)-10;
+					$sel_video=mysqli_query($conn,"SELECT * FROM `video` ORDER BY `write_time` ASC limit ".$n.",10");
+				}else{
+					$sel_video=mysqli_query($conn,"SELECT * FROM `video` ORDER BY `write_time` ASC limit 0,10");
+				}
+				//$sel_video=mysqli_query($conn,"SELECT * FROM `video` ORDER BY `write_time` ASC ");
 				$n = mysqli_num_rows($sel_video);
 				if($n==0){
 					echo "<h1 align='center'><font color='#FF3333'>目前無紀錄資料!!</font></h1>";
@@ -61,5 +81,51 @@
 				}
 			?>
 		</table>
+		<?php
+			//查詢資料筆數
+			$sel_number=mysqli_query($conn,"SELECT * FROM `video`");
+			$data_number=mysqli_num_rows($sel_number);
+			if($data_number!=0){
+				$data_number_remove=intval($data_number/10);
+				$data_number_remain=$data_number%10;
+				$data_number_result=0;
+				//echo "<script>alert('$data_number_remove')</script>";
+				if($data_number_remove<1 && $data_number_remain!=0){
+					$data_number_result=1;
+				}else if($data_number_remove>=1 && $data_number_remain==0){
+					$data_number_result=$data_number_remove;
+				}else if($data_number_remove>=1 && $data_number_remain!=0){
+					$data_number_result=$data_number_remove+1;
+				}
+				echo "
+					<p/>
+				";
+				//if($n!=0){
+					echo "
+						<center>
+							<select id='s1' onChange='qq();' style='font-size: 20px;'>
+					";
+						for($i=1;$i<=$data_number_result;$i++){
+							if(isset($_GET['n'])){
+								if($i==$_GET['n']){
+									echo "<option value='".$i."' selected>第".$i."頁</option>";
+								}else{
+									echo "<option value='".$i."' >第".$i."頁</option>";
+								}
+							}else{
+								if($i==1){
+									echo "<option value='".$i."' selected>第".$i."頁</option>";
+								}else{
+									echo "<option value='".$i."' >第".$i."頁</option>";
+								}
+							}
+						}
+						echo "	
+							</select>
+						</center>
+					";
+				//}
+			}
+		?>
 	</body>
 </html>
