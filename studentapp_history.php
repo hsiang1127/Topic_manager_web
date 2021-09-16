7d403f7d996d49964942dff61d1c7c6f48a8ac60
hsiang1127 <10936002@ntub.edu.tw>
2021-09-16 08:41:00 +0800

    修改功能項目文字

--- studentapp_history.php
+++ studentapp_history.php
@@ -51,7 +51,7 @@
 			<button class='button' id='btu11' name='btu11' onclick="show_apphistory('adaptation_scale_sw_first.php')"><b>社會適應量表填寫紀錄(初)</b></button>
 			<button class='button' id='btu12' name='btu12' onclick="show_apphistory('adaptation_scale_sw_endtest.php')"><b>社會適應量表填寫紀錄(末)</b></button>
 			<button class='button' id='btu2' name='btu2' onclick="show_apphistory('mood_thermometer_w.php')"><b>情緒溫度計填寫紀錄</b></button>
-			<button class='button' id='btu3' name='btu3' onclick="show_apphistory('mood_diary_w.php')"><b>心情日記寫紀錄</b></button>
+			<button class='button' id='btu3' name='btu3' onclick="show_apphistory('mood_diary_w.php')"><b>心情日記填寫紀錄</b></button>
 			<button class='button' id='btu4' name='btu4' onclick="show_apphistory('play_history.php')"><b>遊戲遊玩紀錄</b></button>
 			<button class='button' id='btu5' name='btu5' onclick="show_apphistory('video.php')"><b>影片新增紀錄</b></button>
 		</center>
